7d7c206ec83ac05adc2fd78e7741b64ed8e8939a
DESKTOP-9H88HHD\nikol <nikolas.klaunzer@gmail.com>
2023-02-02 14:22:29 +0100

    Added Show Table and Relationship between models

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ class Venue(db.Model):
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     genres = db.Column(db.String)
+    shows = db.relationship('Show', backref='Venue', lazy=True);
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
@@ -57,11 +58,20 @@ class Artist(db.Model):
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    shows = db.relationship('Show', backref='Artist', lazy=True);
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+    __tablename__ = 'Show'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    date = db.Column(db.Date)
+    time = db.Column(db.Time)
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
 
 
 # ----------------------------------------------------------------------------#
