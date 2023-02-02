8b0b627413d334c6e8d9ff0a9af4a14f21a74bac
DESKTOP-9H88HHD\nikol <nikolas.klaunzer@gmail.com>
2023-02-02 14:26:09 +0100

    Removed Date and Time Column of Show and replaced it with start_time

--- app.py
+++ app.py
@@ -63,13 +63,11 @@ class Artist(db.Model):
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 class Show(db.Model):
     __tablename__ = 'Show'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    date = db.Column(db.Date)
-    time = db.Column(db.Time)
+    start_time = db.Column(db.DateTime)
     artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
     venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
 
